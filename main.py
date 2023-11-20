6eb7165a0cda248afc1c2dba22a93492a896d7ef
armasuv <armasuv@educ.college-montreal.qc.ca>
2023-11-20 10:44:50 -0500

    straight ninja turtle

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #Crée par Victor Ionut Armasu en 2023
 #TP3
 
-#Fonctions:
+#Importation
 
 import random
 import sys
+
+#Fonctions:
 puissance_boss = 0
 vies_boss = 0
 niveau_vie = 20
@@ -13,13 +15,22 @@ nombre_victoires = 0
 nombre_defaites = 0
 nombre_victoires_consecutives = 0
 force_adversaire = 0
+statut_combat = "Victoire"
 god_mode = False
 
-
+#Introduction
 print("Vous êtes dans un monde plein de couloirs, et à chaque bout de couloir se trouve une porte qui cache un ennemi de différents niveaux.")
 print("Vous êtes arrivés au premier combat:")
+
+#fonction qui contient les règle du jeu
 def regles_du_jeu():
-   print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire. Dans ce cas,le niveau de vie de l’usager est augmenté de la force de l’adversaire.\nUne défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.  Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire. \nLa partie se termine lorsque les points de vie de l’usager tombent sous 0. \nL’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
+   print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire."
+    "Dans ce cas,le niveau de vie de l’usager est augmenté de la force de l’adversaire."
+    "\nUne défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire." ""
+    " Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire. \nLa partie se termine lorsque les points de vie de l’usager tombent sous 0. "
+    "\nL’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
+
+#Fonction qui définit le menu
 
 def menu():
    print("Que voulez-vous faire ?\n"
@@ -29,8 +40,7 @@ def menu():
   "4- Quitter la partie\n");
 
 
-
-
+#Commencement de la fonction prinicpale, jeu
 def jeu():
    global niveau_vie
    global numero_combat
@@ -41,13 +51,18 @@ def jeu():
    global vies_boss
    global puissance_boss
    while niveau_vie > 0:
+
+
+        #Mode de jeu avec difficulté normale
+
        if god_mode == False:
 
+           #Détécte si vous avez assez de victoires pour affronter le boss en difficulté normale
 
            if nombre_victoires_consecutives == 3:
                print("\n\nVous êtes arrivez devant un boss!!")
-               print("Maintenant, vous allez echanger des coups et la personne qui a le plus de puissance ne perde pas le nombre de vie qui égale à la puissance de l'advesaire, et le boss a le même nombre de vies que toi: ",niveau_vie)
-               print("Vous gangez un des 10!!!")
+               print("Maintenant, vous allez les deux roulez des dès, et la personne qui a le plus grand montant peu attaquer l'autre avec ce montant. Le boss a le même nombre de vie que toi: ",niveau_vie)
+               print("Vous gangez deux dès 3!!!")
                vies_boss = niveau_vie
                while vies_boss > 0:
                    puissance_boss = random.randint(1, 10)
@@ -74,17 +89,18 @@ def jeu():
                        if niveau_vie <= 0:
                            print("GAME OVER")
                            sys.exit()
+               #Boss finit, le mode de jeu  normal recommence
                print("Bravo! Vous avez battus le boss!\nVous retournez dans le monde des corridors et poursuivaient votre aventure.")
 
 
-
-           force_adversaire = random.randint(1, 5)
+           #Le mode de jeu normal avec les adversaires normals dans les corridors
+           force_adversaire = random.randint(1, 10)
            print("\nLa force de l'adversaire est de", force_adversaire,"\n1"
                                                                        "")
            choix = int(input(menu()))
            if choix == 1:
-               lancer_des = random.randint(1, 3)
-               lancer_des2 = random.randint(1, 3)
+               lancer_des = random.randint(1, 5)
+               lancer_des2 = random.randint(1, 5)
                total_des = lancer_des + lancer_des2
                print("Vous avez roulez:", total_des)
                if total_des > force_adversaire:
@@ -117,16 +133,22 @@ def jeu():
                god_mode = True
                print("GOD MODE ACTIVATED")
                niveau_vie +=10
-               print("Vous avez ", niveau_vie,"vies et la force de l'adversaire est de maximum 1 ;)")
+               print("Vous avez ", niveau_vie,"vies et la force de l'adversaire est de maximum 3 ;)")
 
 
            else:
                print("Bye")
                sys.exit()
+
+       #Mode de jeu facile/god, mais la difficulté du boss n'est pas facile
+
        elif god_mode == True:
+
+           #Détécte si vous avez assez de victoires pour affronter le boss qui possède plus de vies
+
            if nombre_victoires_consecutives == 3:
                print("\n\nVous êtes arrivez devant un boss!!")
-               print("Maintenant, vous allez echanger des coups et la personne qui a le plus de puissance ne perde pas le nombre de vie qui égale à la puissance de l'advesaire, et le boss a le même nombre de vies que toi: ",niveau_vie)
+               print("Maintenant, vous allez les deux roulez des dès, et la personne qui a le plus grand montant peu attaquer l'autre avec ce montant. Le boss a le même nombre de vie que toi: ",niveau_vie)
                print("Vous gangez un des 10!!!")
                vies_boss = niveau_vie
                while vies_boss > 0:
@@ -154,18 +176,20 @@ def jeu():
                        if niveau_vie <= 0:
                            print("GAME OVER")
                            sys.exit()
+               #Boss battus, retours dans le mode de jeu facile avec les corridors
+
                print("Bravo! Vous avez battus le boss!\nVous retournez dans le monde des corridors et poursuivaient votre aventure.")
 
 
 
+           #Mode de jeu facile avec les monstres difficulté facile
 
-
-           force_adversaire= 1
+           force_adversaire= 3
            print("\nLa force de l'adversaire est de", force_adversaire,"\n")
            choix = int(input(menu()))
            if choix == 1:
-               lancer_des = random.randint(1, 3)
-               lancer_des2 = random.randint(1, 3)
+               lancer_des = random.randint(1, 5)
+               lancer_des2 = random.randint(1, 5)
                total_des = lancer_des + lancer_des2
                print("Vous avez roulez:", total_des)
                if total_des > force_adversaire:
@@ -175,8 +199,6 @@ def jeu():
                    nombre_victoires += 1
                    nombre_victoires_consecutives += 1
                    print("combat numéro= ", numero_combat,"\nvie= ", niveau_vie,"\nnombres de défaites =", nombre_defaites,"\nle nombre de victoires =", nombre_victoires,"\n et le nombre de victoires consécutifs: ", nombre_victoires_consecutives)
-
-
                    jeu()
 
                else:
@@ -187,7 +209,17 @@ def jeu():
                    nombre_defaites += 1
                    print("combat numéro= ", numero_combat,"\nvie= ", niveau_vie,"\nnombres de défaites =", nombre_defaites,"\nle nombre de victoires =", nombre_victoires,"\n et le nombre de victoires consécutifs: ", nombre_victoires_consecutives)
 
+           elif choix == 2:
+               print("Vous passez au prochains adversaire en perdant 1 vie.")
+               niveau_vie = niveau_vie -1
+               print(niveau_vie)
+           elif choix == 3:
+               print(regles_du_jeu())
+           else:
+               print("Bye")
+               sys.exit()
 
+#On active la fonction jeu qui constitue tout le programme
 jeu()
 
 
